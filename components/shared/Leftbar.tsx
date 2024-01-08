0605ad4d317d4b2995e73c1d6643dcb4ade32ad5
Abhishek <abhishekparmar162@gmail.com>
2024-01-09 00:21:53 +0530

    fix styling

--- components/shared/Leftbar.tsx
+++ components/shared/Leftbar.tsx
@@ -1,9 +1,11 @@
 import { sidebarLinks } from "@/constants";
 import Link from "next/link";
+import { Button } from "../ui/button";
+import { Twitter } from "lucide-react";
 
 const Leftbar = () => {
   return (
-    <div className="mb-12">
+    <div className="mb-12 mx-5 md:w-64 w-16  h-full">
       {sidebarLinks.map((bar) => (
         <Link href={bar.route} key={bar.route}>
           <div className="flex items-center space-x-4 space-y-8 justify-start ">
@@ -12,6 +14,13 @@ const Leftbar = () => {
           </div>
         </Link>
       ))}
+      <Button
+        className="bg-blue-500 w-full hidden md:block mt-6 rounded-full mr-20 text-white"
+        variant={"ghost"}
+      >
+        Post
+      </Button>
+      <Twitter size={30} className="md:hidden mt-6 text-blue-500" />
     </div>
   );
 };
