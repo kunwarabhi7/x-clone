4daad8389368e764130b2a703b7a9e4899d060aa
Abhishek <abhishekparmar162@gmail.com>
2024-01-10 17:22:55 +0530

    added new user

--- components/shared/Leftbar.tsx
+++ components/shared/Leftbar.tsx
@@ -1,9 +1,12 @@
 import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { Twitter } from "lucide-react";
+import { MoreHorizontal, Twitter } from "lucide-react";
+import { currentUser, SignOutButton } from "@clerk/nextjs";
+import Image from "next/image";
 
-const Leftbar = () => {
+const Leftbar = async () => {
+  const user = await currentUser();
   return (
     <div className="mb-12 mx-5 md:w-48 w-16  h-full">
       {sidebarLinks.map((bar) => (
@@ -21,6 +24,30 @@ const Leftbar = () => {
         Post
       </Button>
       <Twitter size={30} className="md:hidden mt-6 text-blue-500" />
+      <div className=" flex items-center space-x-3 mt-12 hover:bg-gray-700 rounded-full p-2 cursor-pointer w-full ">
+        <div>
+          <Image
+            src={user?.imageUrl!}
+            alt={user?.firstName!}
+            className="rounded-full w-12 h-12"
+            width={20}
+            height={20}
+            priority
+          />
+        </div>
+        <div className="ml-4">
+          <div className="flex ">
+            <h1>{user?.firstName} </h1>
+            <h1>{user?.lastName} </h1>
+          </div>
+          <h1 className="text-gray-500 ml-2">@{user?.username}</h1>
+        </div>
+        <SignOutButton>
+          <div className="ml-2">
+            <MoreHorizontal />
+          </div>
+        </SignOutButton>
+      </div>
     </div>
   );
 };
